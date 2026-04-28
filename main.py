f9579e28a51926a9785fd2d394226d165fd37056
capacitacionarquitecto-hub <capacitacion.arquitecto@gmail.com>
2026-04-28 14:55:45 -0500

    iam commit message for the changes made to main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url='http://localhost:11434/v1',
+    api_key='ollama'
+)
+
+resp = client.chat.completions.create(
+    model='qwen3.5:397b-cloud',
+    messages=[
+        {
+        "role": "system", 
+        "content": "You are a helpful assistant."
+        }
+        ,{
+        "role": "user", 
+        "content": "hello, dime quien eres?"
+        }
+
+    ]
+)
+
+print(resp.choices[0].message.content)
 print('My First Agent is Running!')
